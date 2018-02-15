2c0b74a9456e06424280676137acc52a29bb767f
Ben Tasker <btasker@swiftserve.com>
2018-02-15 13:52:33 +0000

    Creates greasemonkey script for STGNG-7
    I had originally planned to load the anti-amp Javascript direct from Github (using raw.) but unfortunately the browser refuses to run it because the content-type is returned as text/plain rather than application/javascript
    
    Instead, we serve it via my CDN.
    
    When adding the script anchor, we include a SubResourceIntegrity (SRI) hash to minimise the chances of a MITM (or compromise of my system) buggering anyone.

---
+++ greasemonkey_hook.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name           AMP Removal script hook
+// @namespace      http
+// @description    Hooks in the AMP removal javascript to attempt to direct to a proper page
+// @include        *://*/*
+// @run-at document-end
+// @grant none
+//
+// Author: B Tasker
+// ==/UserScript==
+
+function RemoveAmpTrigger(){
+
+var head = document.getElementsByTagName("head")[0];
+var an = document.createElement('script');
+// Was gonna serve direct from github, but Chrome complains about the mime-type (github specifies text/plain)
+an.setAttribute('src','https://static1.bentasker.co.uk/adblock/anti-amp/v1.js');
+an.setAttribute('type','text/javascript');
+
+// We use SRI to help ensure we don't end up fetching a compromised file
+an.setAttribute('integrity',"sha384-9vGiR/wn+A688mGafpv/bxCzCIqaOWMlHzmfD2EWr/oWP/4+29SlUg4UWynL4eoC");
+an.setAttribute('crossorigin',"anonymous");
+
+
+head.appendChild(an);
+
+}
+
+
+
+
+var b = document.getElementsByTagName('body')[0];
+var s = document.createElement('script');
+s.setAttribute('type','text/javascript');
+s.appendChild(document.createTextNode('('+ RemoveAmpTrigger +')();'))
+b.appendChild(s);
