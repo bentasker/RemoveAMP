cfffdf43451320a13045755124ed9a8e862f37b3
B Tasker <github@bentasker.co.uk>
2018-02-15 15:30:18 +0000

    MISC-25 Switch to using TM/GM's require directive
    This allows TamperMonkey to run the script even on pages with a strict Content-Security-Policy (as TM supports adding itself into any CSP headers which are present).
    
    It also results in faster trigger times, and means my CDN's logs now won't contain referrer strings showing peoples browsing history (which is a win for both them and me)

--- greasemonkey_hook.user.js
+++ greasemonkey_hook.user.js
@@ -8,31 +8,10 @@
 // @downloadURL https://github.com/bentasker/RemoveAMP/raw/master/greasemonkey_hook.user.js
 // @version 1.1
 //
+// @require https://static1.bentasker.co.uk/adblock/anti-amp/v1.1.js#sha384=wyofJiXdjNwcrIS22S2Cb+yvDgowQPXmcfCJQhcuvIQhaLubXP9xTTwecjOv6wUf
+//
+//
 // Author: B Tasker
 // ==/UserScript==
 
-function RemoveAmpTrigger(){
-
-var head = document.getElementsByTagName("head")[0];
-var an = document.createElement('script');
-// Was gonna serve direct from github, but Chrome complains about the mime-type (github specifies text/plain)
-an.setAttribute('src','https://static1.bentasker.co.uk/adblock/anti-amp/v1.js');
-an.setAttribute('type','text/javascript');
-
-// We use SRI to help ensure we don't end up fetching a compromised file
-an.setAttribute('integrity',"sha384-wyofJiXdjNwcrIS22S2Cb+yvDgowQPXmcfCJQhcuvIQhaLubXP9xTTwecjOv6wUf");
-an.setAttribute('crossorigin',"anonymous");
-
-
-head.appendChild(an);
-
-}
-
-
-
-
-var b = document.getElementsByTagName('body')[0];
-var s = document.createElement('script');
-s.setAttribute('type','text/javascript');
-s.appendChild(document.createTextNode('('+ RemoveAmpTrigger +')();'))
-b.appendChild(s);
+fuckOffAMP();
