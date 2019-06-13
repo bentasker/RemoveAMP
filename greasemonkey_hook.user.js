6cb28eb8214b489f060424442f6adf3e178c296b
B Tasker <github@bentasker.co.uk>
2019-06-13 16:52:33 +0100

    FKAMP-8 Bump version number
    The only purpose of this commit (and release) is to move version numbers to having 2 sets of numbers rather than 3
    
    The reason being that Greasemonkey has been claiming no updates are available (though that's partly because I ballsed up the numbering)

--- greasemonkey_hook.user.js
+++ greasemonkey_hook.user.js
@@ -6,7 +6,7 @@
 // @run-at document-end
 // @grant none
 // @downloadURL https://github.com/bentasker/RemoveAMP/raw/master/greasemonkey_hook.user.js
-// @version 1.4.3
+// @version 1.5
 //
 // @require https://static1.bentasker.co.uk/adblock/anti-amp/v1.4.3.js#sha384=lNjJABp+iorW1BqUjEM7a/WYaIxB2NyzZJHCmc1Gea1GNi+y++STvz6u7S7LInIP
 //
